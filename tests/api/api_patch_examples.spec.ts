d04bc2ccb9cc799ac598bf8601bec016a780853b
Estefanía Fdez Muñoz <estefafdez@gmail.com>
2022-12-05 12:39:00 +0100

    test: add patch tests

---
+++ tests/api/api_patch_examples.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("API PATCH 200 Request", () => {
+  test("[14, API] should get a 200 response after a PATCH request to update the user data", async ({
+    request,
+    baseURL,
+  }) => {
+    const response = await request.patch(`${baseURL}/api/users/2`, {
+      data: {
+        body: {
+          name: "morpheus",
+          job: "zion resident",
+        },
+      },
+    });
+
+    const responseBody = await response.json();
+
+    expect(response.status()).toBe(200);
+    console.log(responseBody);
+    expect(responseBody.body.name).toEqual("morpheus");
+    expect(responseBody.body.job).toEqual("zion resident");
+    expect(responseBody).toHaveProperty("updatedAt");
+  });
+});
