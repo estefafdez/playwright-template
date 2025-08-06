222def49b53dde1f93e5e49d78c075ef57b86700
Estefania Fdez <estefafdez@gmail.com>
2025-08-06 10:43:49 +0200

    refactor(e2e): update home page logo locator to use role-based selector

--- elements/HomeElements.ts
+++ elements/HomeElements.ts
@@ -6,7 +6,7 @@ export class HomeElements {
 
   constructor(page: Page) {
     this.page = page;
-    this.pageLogo = this.page.locator("img[alt='QA Automation Labs']");
+    this.pageLogo = this.page.getByRole("img", { name: "QA Automation Labs" });
   }
 }
 
